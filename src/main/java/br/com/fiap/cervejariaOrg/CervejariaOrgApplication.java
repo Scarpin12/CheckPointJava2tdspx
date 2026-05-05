a5492cd7679c0939cec1836139d3d301c9b4f04d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-05 19:52:52 -0300

    feat- estrutura inicial do projeto

---
+++ src/main/java/br/com/fiap/cervejariaOrg/CervejariaOrgApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cervejariaOrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervejariaOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervejariaOrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/cervejariaOrg/CervejariaOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cervejariaOrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervejariaOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
